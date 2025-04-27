05a47286bac61e6ffb3f1c9e9d7c8eded81bae25
Maxim <maxim@odenweller.xyz>
2025-04-27 23:15:09 +0200

    test

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 # === CONFIG VARIABLES ===
 DISK="/dev/sda"
-LVM_GROUP_NAME="test"
+PV_NAME="pvname"
+LVM_GROUP_NAME="lvname"
 SWAP_SPACE="1G" # example: "8G"
 ROOT_SPACE="4G" # example: "64G"
 
@@ -30,12 +31,12 @@ parted --script "$DISK" mkpart primary 1025MiB 100%
 # === ENCRYPT LVM PARTITION ===
 echo "Encrypting partition..."
 cryptsetup luksFormat --batch-mode "${DISK}2"
-cryptsetup open "${DISK}2" cryptlvm
+cryptsetup open "${DISK}2" "$PV_NAME"
 
 # === SETUP LVM ===
 echo "Setting up LVM..."
-pvcreate /dev/mapper/cryptlvm
-vgcreate "$LVM_GROUP_NAME" /dev/mapper/cryptlvm
+pvcreate /dev/mapper/$PV_NAME
+vgcreate "$LVM_GROUP_NAME" /dev/mapper/$PV_NAME
 
 lvcreate -L "$SWAP_SPACE" -n swap "$LVM_GROUP_NAME"
 lvcreate -L "$ROOT_SPACE" -n root "$LVM_GROUP_NAME"
